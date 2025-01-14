3f0b0afdad4c5ceb80b83c5c3e2883601bb43fe4
José Páez <78059182+Josepaezra@users.noreply.github.com>
2025-01-14 13:08:19 -0400

    Add files via upload

---
+++ Asubtitler/test.py
@@ -0,0 +1,29 @@
+import os
+import ffmpeg
+import whisper
+import argparse
+import warnings
+import tempfile
+from utils import filename, str2bool, write_srt
+
+
+
+def get_audio(paths):
+    temp_dir = tempfile.gettempdir()
+
+    audio_paths = {}
+
+    for path in paths:
+        print(f"Extracting audio from {filename(path)}...")
+        output_path = os.path.join(temp_dir, f"{filename(path)}.wav")
+
+        ffmpeg.input(path).output(
+            output_path,
+            acodec="pcm_s16le", ac=1, ar="16k"
+        ).run(quiet=True, overwrite_output=True)
+
+        audio_paths[path] = output_path
+
+    return audio_paths
+
+
